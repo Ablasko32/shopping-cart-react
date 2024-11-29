b2a53f973b2fe8eba987a2083f72b56d1379a539
ante <an.blaskovic@gmail.com>
2024-11-30 00:03:15 +0100

    fixes for deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <div className="text-center bg-violet-200 py-3 text-sm font-thin">
+    <div className="text-center bg-violet-200 py-3 text-sm font-thin mt-2">
       <p>Copyright &copy;{year}</p>
       <p>Antonio Blašković</p>
     </div>
