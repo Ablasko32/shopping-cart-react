72f5aaf6f63dfe44ebc919e59448e5f575dea7ae
ante <an.blaskovic@gmail.com>
2024-11-30 13:19:06 +0100

    fixed mail

--- src/components/MailButton.jsx
+++ src/components/MailButton.jsx
@@ -2,12 +2,13 @@ import { useList } from "../contexts/listContext";
 
 function MailButton() {
   const { toBuy } = useList();
-  const names = toBuy.map((el) => `x${el.quantity} ${el.name} | ${el.store}`);
+  const names = toBuy?.map((el) => `x${el.quantity} ${el.name} | ${el.store}`);
 
-  const mailBody = `Popis za trgovinu!\nDatum:${new Date().toDateString()}\n\n ${names.join("\n")}\n\nPowered By CartMan`;
+  const mailBody = `Popis za trgovinu!\nDatum:${new Date().toDateString()}%0D%0A%0D%0A ${names.join("%0D%0A")}%0D%0A%0D%0APowered By CartMan`;
   const mailSubject = "Novi popis za trgovinu";
 
   function handleMail() {
+    if (toBuy.length === 0) return;
     window.location.href = `mailto:?subject=${encodeURIComponent(mailSubject)}&body=${encodeURIComponent(mailBody)}`;
   }
 
